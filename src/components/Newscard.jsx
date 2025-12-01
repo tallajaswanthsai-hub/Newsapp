ea666d11d355b310357a6821516d6a572dfdadf7
jaswanth sai <tallajaswanthsai@gmail.com>
2025-12-01 18:23:06 +0530

    api project

--- src/components/Newscard.jsx
+++ src/components/Newscard.jsx
@@ -4,7 +4,10 @@ const Newscard = ({title,description,img,url}) => {
   return (
     
     <div className="card bg-dark text-light mb-3  d-inline-block my-3 mx-3 p-2 " style={{maxWidth:"345px"}}>
-  <img  src={img ? img : image}  style={{height:"200px", width:"330px"}} className="card-img-top" alt="..."/>
+  <img  src={img ? img : image} onError={(e)=>{e.target.onerror =null;
+    e.target.src=image}
+  } style={{height:"200px", width:"330px"}} className="card-img-top" alt="..."/>
+  
   <div className="card-body">
     <h5 className="card-title">{title.slice(0,50)}</h5>
     <p className="card-text">{description?description.slice(0,90):"This is the current event.it is information about something that has just happened."}</p>
